77b1651683846b0c72afa941849354853c05a8fe
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-06-17 19:19:26 +0600

    gjgh

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -106,6 +106,29 @@ $(document).ready(function () {
     });
 
 
+    //submenu visible area start
+    $(".sidenavitem").click(function() {
+        $(this).find(".submenuarrow").toggleClass("rotated");
+    });
+    
+    $("#navmenuitem1").click(function(){
+        $("#submenu1").slideToggle();
+    });
+    $("#navmenuitem2").click(function(){
+        $("#submenu2").slideToggle();
+    });
+    $("#navmenuitem3").click(function(){
+        $("#submenu3").slideToggle();
+    });
+    $("#navmenuitem4").click(function(){
+        $("#submenu4").slideToggle();
+    });
+    $("#navmenuitem5").click(function(){
+        $("#submenu5").slideToggle();
+    });
+    $("#navmenuitem6").click(function(){
+        $("#submenu6").slideToggle();
+    });
 
 
 });
